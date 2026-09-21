7c3a45fbbafbb415e369ece2adb6a9fdc4025ef2
Dborasik <26031922+Dborasik@users.noreply.github.com>
2026-09-20 20:23:26 -0400

    Revert "feat: report collector capabilities (#3)"
    This reverts commit fb2dd853e854e1f7fe85624f0d99bce0f1405a9a.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ game 'gta5'
 name 'guildrate-collector'
 author 'GuildRate'
 description 'Drop-in analytics collector: reports sessions, players and framework events to the GuildRate dashboard'
-version '0.2.0'
+version '0.1.2'
 
 shared_scripts {
     'config.lua'
